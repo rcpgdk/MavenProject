ef384571e69ed952b1718784e13f6bc6977101b2
Recep GUDEK <rcpgdk590@gmail.com>
2021-06-10 15:23:12 +0200

    update added more hello feature

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,7 +3,8 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println("Hello");
-
+        System.out.println("Local Repo has already published on Github");
+        System.out.println("Doing more work");
     }
 
 }
