aee52be2741a807ecc5b29b26fd2743b0edb24e6
Recep GUDEK <rcpgdk590@gmail.com>
2021-06-10 15:14:04 +0200

    started hello feature

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -3,6 +3,7 @@ public class HelloTest {
     public static void main(String[] args) {
 
         System.out.println("HelloTest");
+        System.out.println("Developing hello feature");
 
     }
 }
